0b7bd2aae2e0c1662775bfb5fa3990b3eca8254f
thalyshenrique7 <thalys_henrique@outlook.com>
2023-06-26 07:27:43 -0300

    build: ApiErrors and ExceptionHandler updated.

--- src/main/java/com/devthalys/inventorycontrolsystem/rest/controllers/exceptionHandler/ApplicationControllerAdvice.java
+++ src/main/java/com/devthalys/inventorycontrolsystem/rest/controllers/exceptionHandler/ApplicationControllerAdvice.java
@@ -7,10 +7,11 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.*;
 
 @RestControllerAdvice
 public class ApplicationControllerAdvice {
@@ -28,72 +29,82 @@ public class ApplicationControllerAdvice {
     }
 
     @ExceptionHandler(ProductAlreadyExistsException.class)
-    @ResponseStatus(BAD_REQUEST)
+    @ResponseStatus(CONFLICT)
     public ApiErrors handleProductAlreadyExistsException(ProductAlreadyExistsException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
-    @ResponseStatus(BAD_REQUEST)
+    @ResponseStatus(NOT_FOUND)
     public ApiErrors handleProductNotFoundException(ProductNotFoundException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 
     @ExceptionHandler(UserException.class)
-    @ResponseStatus(BAD_REQUEST)
+    @ResponseStatus(UNAUTHORIZED)
     public ApiErrors handleUnauthorizedAccessException(UserException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 
     @ExceptionHandler(ValueInvalidException.class)
-    @ResponseStatus(BAD_REQUEST)
+    @ResponseStatus(NOT_FOUND)
     public ApiErrors handleBalanceInvalidException(ValueInvalidException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 
     @ExceptionHandler(SaveMovementException.class)
     @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleSaveMovementException(SaveMovementException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 
     @ExceptionHandler(InventoryException.class)
     @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleInventoryException(InventoryException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 
     @ExceptionHandler(InvoiceException.class)
     @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleInvoiceException(InvoiceException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 
     @ExceptionHandler(BinException.class)
     @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleBinException(BinException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 
     @ExceptionHandler(ClientException.class)
     @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleClientException(ClientException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 
     @ExceptionHandler(ShoppingCartException.class)
     @ResponseStatus(BAD_REQUEST)
     public ApiErrors handleShoppingCartException(ShoppingCartException e){
         String errorMessage = e.getMessage();
-        return new ApiErrors(errorMessage);
+        LocalDateTime timeNow = LocalDateTime.now();
+        return new ApiErrors(errorMessage, timeNow);
     }
 }
--- src/main/java/com/devthalys/inventorycontrolsystem/rest/errorsMessages/ApiErrors.java
+++ src/main/java/com/devthalys/inventorycontrolsystem/rest/errorsMessages/ApiErrors.java
@@ -2,6 +2,7 @@ package com.devthalys.inventorycontrolsystem.rest.errorsMessages;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,12 +10,14 @@ import java.util.List;
 public class ApiErrors {
 
     private List<String> errors;
+    private LocalDateTime time;
 
     public ApiErrors(List<String> errors) {
         this.errors = errors;
     }
 
-    public ApiErrors(String errorMessage){
+    public ApiErrors(String errorMessage, LocalDateTime time){
         this.errors = Arrays.asList(errorMessage);
+        this.time = time;
     }
 }
